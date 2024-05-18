5901e323833a1cbb78dbd3a2a1c84c6e5c2c1d16
aaravjaichand <aaravj413@gmail.com>
2024-05-18 18:27:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from tqdm import tqdm
-
+from sklearn.ensemble import RandomForestClassifier
 
 def preposition(string):
     prepositions = [
@@ -37,6 +37,9 @@ def upperLower(string):
                 upper += 1
             else:
                 lower += 1
+    if lower == 0 or upper == 0:
+        return 0
+    
     return upper / lower
 
 
@@ -48,6 +51,7 @@ def articles(string):
         wordList.count("A") + wordList.count("a")
     if the_a_count == 0 or sentenceWC == 0:
         return 0
+    
     articleRatio = the_a_count / sentenceWC
 
     return articleRatio
@@ -74,7 +78,7 @@ def main():
     str_inputs = []
     targets = []
 
-    rAmount = 60000
+    rAmount = 50000
     test_size = int(rAmount * .1)
 
     for i, line in tqdm(
@@ -92,16 +96,17 @@ def main():
         [feature_func(inp) for feature_func in feature_funcs]
         for inp in tqdm(str_inputs, desc='Processing features...')
     ])
-    # np.save("inputs.out.npy", inputs)
-    targets = np.array(targets)
-    # np.save("targets.out.npy", targets)
-    classifier = MLPClassifier(random_state=3, hidden_layer_sizes=(
-        50, 10, 50), learning_rate_init=0.003, batch_size=60000, verbose=1, max_iter=300)
-    # np.load("inputs.out.npy", inputs)
-    # np.load("targets.out.npy", targets)
+
+    # targets = np.array(targets)
+    # m = RandomForestClassifier(
+    #     random_state=12, n_estimators=70, max_depth=5, verbose=1)
+    # m.fit(inputs[test_size:], targets[test_size:])
+    # results = m.predict(inputs[:test_size])
+
+    classifier = MLPClassifier(random_state=1, hidden_layer_sizes=(
+        10, 10, 50), learning_rate_init=0.005, batch_size=test_size, max_iter=300, verbose=1)
     classifier.fit(inputs[test_size:], targets[test_size:])
-    print(classifier.loss_curve_)
-    
+    print("Min loss:", min(classifier.loss_curve_))
     results = classifier.predict(inputs[:test_size])
 
     print(f'Accuracy: {np.mean(results == targets[:test_size])}')
