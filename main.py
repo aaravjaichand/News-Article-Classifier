d81ff1433c0f794297b2a694d3d7e533bb8b6fa8
aaravjaichand <aaravj413@gmail.com>
2024-06-03 19:58:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import matplotlib.pyplot as plt
+from transformers import pipeline
+
 
 def preposition(string):
     prepositions = [
@@ -75,14 +77,15 @@ def avg(string):
 
 def sentenceLen(string):
     return len(string.split())
-
+acceptedCats = ["WORLD NEWS", "POLITICS", "ENTERTAINMENT"]
 def get_data():
     saved_file = Path('saved_data.npz')
-    acceptedCats = ["WORLD NEWS", "POLITICS", "ENTERTAINMENT"]
-    if not saved_file.exists():
-        
-        targets = []
 
+
+    if not saved_file.exists():
+        inputs = []
+        targets = []
+        list_of_strings = []
         for line in tqdm(
                 list(open("News_Category_Dataset_v3.json", "r")),
                 desc='Loading json...'
@@ -93,6 +96,7 @@ def get_data():
             headline = data["headline"]
             short_desc = data["short_description"]
             if category in acceptedCats:
+                list_of_strings.append(headline + '. ' +short_desc)
                 inputs.append(headline + '. ' +short_desc)
                 targets.append(category)
 
@@ -103,12 +107,13 @@ def get_data():
             for inp in tqdm(inputs, desc='Processing features...')
         ])
         targets = np.array(targets)
-        np.savez(saved_file, inputs=inputs, targets=targets)
+        np.savez(saved_file, inputs=inputs, targets=targets, list_of_strings=list_of_strings)
     else:
         arr = np.load(saved_file)
         inputs = arr['inputs']
         targets = arr['targets']
-    return inputs, targets
+        list_of_strings = arr["list_of_strings"]
+    return inputs, targets, list_of_strings
 
 
 def display_accuracy(target, predictions, labels, plot_title):
@@ -120,42 +125,48 @@ def display_accuracy(target, predictions, labels, plot_title):
     ax.set_title(plot_title)
     plt.show()
 def main():
-    inputs, targets = get_data()
+    inputs, targets, list_of_strings = get_data()
+    test_size = int(len(inputs) * 0.1)
 
-    
+    # Random Forest Classifier:
     # m = RandomForestClassifier(
     #     random_state=12, n_estimators=70, max_depth=5, verbose=1)
     # m.fit(inputs[test_size:], targets[test_size:])
     # results = m.predict(inputs[:test_size])
 
-    test_size = int(len(inputs) * 0.1)
+    # MLP Classifier
+    # lrs = np.logspace(-4, -1, 4)
+    # accs = []
+    # i = 1
+    # for lr in lrs:
+    #     classifier = MLPClassifier(random_state=1, hidden_layer_sizes=(
+    #     10, 10, 50), learning_rate_init=lr, batch_size=test_size, max_iter=20, verbose=1)
+    #     classifier.fit(inputs[test_size:], targets[test_size:])
+
+
+    #     results = classifier.predict(inputs[:test_size])
+    #     print(i, "of", len(lrs))
+    #     i+=1
+    #     acc = np.mean(results == targets[:test_size])
+    #     accs.append(acc)
+    # plt.plot(lrs, accs)
+    # plt.show()
+
+    # optimalLR = lrs[accs.index(max(accs))]
+    # print("Best LR tested was", optimalLR)
+    # classifier = MLPClassifier(random_state=1, hidden_layer_sizes=(
+    #     10, 10, 50), learning_rate_init=optimalLR, batch_size=test_size, max_iter=20, verbose=1)
+    # classifier.fit(inputs[test_size:], targets[test_size:])
+    # results = classifier.predict(inputs[:test_size])
+    # display_accuracy(targets[:test_size], results, np.unique(targets), "Confusion Matrix (Close to view accuracy)")
+    # print("Min loss:", min(classifier.loss_curve_))
     
 
-    lrs = np.logspace(-4, -1, 4)
-    accs = []
-    i = 1
-    for lr in lrs:
-        classifier = MLPClassifier(random_state=1, hidden_layer_sizes=(
-        10, 10, 50), learning_rate_init=lr, batch_size=test_size, max_iter=20, verbose=1)
-        classifier.fit(inputs[test_size:], targets[test_size:])
+    model = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
+    results = model.predict(list_of_strings, labels=np.unique(targets))
 
-
-        results = classifier.predict(inputs[:test_size])
-        print(i, "of", len(lrs))
-        i+=1
-        acc = np.mean(results == targets[:test_size])
-        accs.append(acc)
-    plt.plot(lrs, accs)
-    plt.show()
-
-    optimalLR = lrs[accs.index(max(accs))]
-    print("Best LR tested was", optimalLR)
-    classifier = MLPClassifier(random_state=1, hidden_layer_sizes=(
-        10, 10, 50), learning_rate_init=optimalLR, batch_size=test_size, max_iter=20, verbose=1)
-    classifier.fit(inputs[test_size:], targets[test_size:])
-    results = classifier.predict(inputs[:test_size])
-    display_accuracy(targets[:test_size], results, np.unique(targets), "Confusion Matrix (Close to view accuracy)")
-    print("Min loss:", min(classifier.loss_curve_))
+    
+    
     print(f'Accuracy: {np.mean(results == targets[:test_size])}')
 
 
